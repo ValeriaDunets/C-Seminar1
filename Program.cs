f68c95566c987d457d8fabd70fdf565333e5a326
Valeria <valeriadunets>
2023-04-23 17:45:52 +0900

    Seminar1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿//Задача 2
+/*Console.WriteLine ("Input first number");
+int number_1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Input your second number");
+int number_2 = Convert.ToInt32 (Console.ReadLine ());
+if (number_1 > number_2)
+{
+    Console.WriteLine ($"Your {number_1} is max");
+    Console.WriteLine ($"Your second {number_2} is min");
+}
+else
+{
+    Console.WriteLine ($"Your second {number_2} is max");
+    Console.WriteLine ($"Your first {number_1} is min");
+}
+*/
+
+// Задача 4
+/*Console.WriteLine ("Input first number");
+int num1 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Input second number");
+int num2 = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Input third number");
+int num3 = Convert.ToInt32 (Console.ReadLine ());
+int max = 0;
+if (num1 > max)
+{
+    max = num1;
+}
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine ($"Your number {max} is max");
+*/
+
+// Задача 6
+
+/*Console.WriteLine ("Input your number");
+int number = Convert.ToInt32 (Console.ReadLine ());
+int even = number % 2;
+if (even == 0)
+{
+    Console.WriteLine ($"Your {number} is a even number");
+}
+else 
+Console.WriteLine ($"Your number {number} is not even ");
+*/
+
+/*// Задача 8
+
+Console.WriteLine ("Input your number");
+int number_N = Convert.ToInt32 (Console.ReadLine ());
+int even = number_N % 2;
+int current_i = 0;
+while (current_i <= number_N)
+{
+    Console.Write (current_i + " ");
+    current_i = current_i + 2;
+}
+*/
